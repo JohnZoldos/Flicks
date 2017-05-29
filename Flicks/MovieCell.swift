a6b06ccbb5f18351aa9662fe6f808d3439118959
johnzoldos <john.a.zoldos@gmail.com>
2017-05-29 14:44:56 -0400

    App now lists titles and overviews for each movie that is now playing. Uses the movie database.

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by John Zoldos on 5/29/17.
+//  Copyright © 2017 John Zoldos. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overview: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
